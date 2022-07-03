403a44609c55a49b007baf05a914a83a0b2be01a
Daniel Rodríguez Fernández <90853739+idanirf@users.noreply.github.com>
2022-07-04 01:59:38 +0200

    Initial Temas 1, 2 y 3

---
+++ Temas-1-2-3/Ejemplos/Ejemplos/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+/**
+ * @author idanirf
+ * @date 04/07/2022
+ */
+
+//En Kotlin kas funciones son ciudadanos de 1ª Clase, por lo tanto, las funciones son un tipo de dato
+fun main(args: Array<String>) {
+    println("Protección/Mutabilidad/Variables de solo lectura o variables de lectura y escritura")
+    println("val -> variables de solo lectura")
+    println("var -> variables de lectura y escritura")
+    println("CONSTANTES const val -> variables de solo lectura")
+    println("\n")
+    println("Tipos de Datos: ")
+    println("Con números")
+}
---
+++ Temas-1-2-3/readme.md
@@ -0,0 +1,6 @@
+# Temas 1, 2 y 3
+## Tema 1: Kotlin - Introducción
+
+## Tema 2: Fundamentos del Lenguaje
+
+## Tema 3: Definción de Funciones
