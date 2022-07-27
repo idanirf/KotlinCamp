dae636daa353f2b93c3fd7cfb8bbf7eda13b8dc9
Daniel Rodríguez Fernández <daniel.rodriguezfernandez@alumno.iesluisvives.org>
2022-07-27 13:02:55 +0200

    Tema 4 - AlumnosPOO v2

--- Tema-4/Ejemplos/AlumnosPOO/src/main/kotlin/App.kt
+++ Tema-4/Ejemplos/AlumnosPOO/src/main/kotlin/App.kt
@@ -3,7 +3,7 @@ fun main() {
     println("")
 
     val alumno1 = Alumno()
-    println("Alumno 1-A: ${alumno1.name}, ${alumno1.edad}, ${alumno1.curso}")
+    println("Alumno 1-A: ${alumno1.name} ${alumno1.apellidos} ${alumno1.edad} ${alumno1.curso}")
     println("")
     println("Alumno 2-A: ${alumno1.toPrint()} ")
 }
