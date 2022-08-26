f968eccab317ebe5424702b7cb5d7fe875550024
Daniel Rodríguez Fernández <90853739+idanirf@users.noreply.github.com>
2022-08-26 13:15:03 +0200

    Programación Funcional 2.1

--- Tema-5/Ejemplos/src/main/kotlin/Tema5-Funciones/Tema5-ProgramacionFuncional.kt
+++ Tema-5/Ejemplos/src/main/kotlin/Tema5-Funciones/Tema5-ProgramacionFuncional.kt
@@ -13,7 +13,7 @@ módulo de una división.
 Ahora vamos a crear la función, pero esta vez la almacenamos en una variable.
  */
 
-val modulo = fun(numA: Int, numB: Int): Int {
+val moduloA = fun(numA: Int, numB: Int): Int {
     return numA % numB
 }
 /*
@@ -22,8 +22,24 @@ y el tipo de dato que devuelve.
 En este caso le estamos indicando el comportamiento que tiene.
  */
 
-var modulo2: (Int, Int) -> Int = modulo
+var modulo2: (Int, Int) -> Int = moduloA
 
 /*
-
+Ahora creamos una función la cual indicamos su comportamiento en base al tipo de dato que queremos.
+a y b serán los datos que utilicemos en la función y con los que vamos a operar, serán de tipo entero.
  */
+
+var modul: (Int, Int) -> Int = { a, b -> a % b }
+var modul2 = { a: Int, b: Int -> a % b }
+
+
+/*
+Creamos nuestro main y probamos las funciones que hemos creado anteriormente introducimos unos datos aleatorios
+y si hacemos una comprobación con la calculadora podemos comprobar que el resultado es el mismo.
+ */
+fun main() {
+    println(moduloA(10, 4))
+    println(modulo2(10, 3))
+    println(modul(10, 10))
+    println(modul2(10, 3))
+}
